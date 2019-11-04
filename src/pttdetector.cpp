21376f15f7ee08271dc4d759f0a245c89ae50384
Diego Cirilo <dvcirilo@gmail.com>
2019-11-04 18:15:11 -0300

    WIP: translating classes

--- src/pttdetector.cpp
+++ src/pttdetector.cpp
@@ -1,8 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <math.h>
+#include <cmath>
+#include <array>
+using namespace std;
 
-class pttdetector{
+class PttDetector
+{
 
     //pttDetector
     //Receive as input a sequence of 2048 input samples
@@ -15,18 +18,25 @@ class pttdetector{
         const int   bw0Hz         = 1600;   // signal bandwidth in Hz
         const int   bw1Hz         = 2400;   // signal bandwidth in Hz
         const int   bw2Hz         = 3200;   // signal bandwidth in Hz
-        const int   worstCNO      = 40;     // Minimum detectable Carry to N0 dB ratio (aprox 12.7 Eb/N0)  
+        const int   worstCNO      = 40;     // Minimum detectable Carry to N0 
+                                            // dB ratio (aprox 12.7 Eb/N0)  
 
         int  nDecoder;     // Number of decoders
         int  window;       // DFT window
         int  freqW;        // Frequency resolution in number of bits
         int  threshold;    // Detection threshold in Amplitude
-        int  bw0;          // Signal bandwidth in multiple of sampleRate/dftLength
+        int  bw0;          // Signal bandwidth in multiple of
+                                        // sampleRate/dftLength
         int  bw1;          // Signal bandwidth upto 5th harmonic
         int  bw2;          // Signal bandwidth upto 7th harmonic
         int  prevPass;
-        pttdetector(param);
+
+        PttDetector(param);
 
 };
 
-pttdetector(param)
+PttDetector::PttDetector(SimParam param)
+{
+    
+}
+
