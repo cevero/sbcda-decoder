a4556220c45a5b8c17767b36adaeca64e111f408
Reinaldo Agostinho <reinaldoasf@gmail.com>
2019-11-19 18:43:37 -0300

    [WIP] creating the simparam definitive class on hpp

---
+++ src/simparam.hpp
@@ -0,0 +1,31 @@
+#include"PttA2.hpp"
+
+typedef class SimParam{
+float noDbFs = -118.5;
+int fullScaleAmp = 32767;
+int maxPwrDbN0 = 64;
+int dynRangeDb = 24;
+int peakPwrDbN0 = 84;
+int nPtt = 50;
+list<float>pwrDbN0List;
+list<float>thetaDegList;
+list<float>freqHzList;
+list<float>hzPerSecList;
+for (int i = 0; i < nPtt; i++) {
+    pwrDbN0List.push_back(something);
+    thetaDegList;
+    freqHzList;
+    hzPerSecList;
+}
+int tSim = 10;
+list<float>timeList;
+int vectSignalGen =0;
+int nDecoder = 12;
+int freqW = 20;
+list<int>typeList;
+list<int>userMsgLenList;
+long fs = 128000;
+
+list<PttA2>pttList;
+
+}
