a58fd40446850b0426f49bf57abc878946f0e52b
reinaldoasf <reinaldoasf@gmail.com>
2020-05-27 12:22:10 -0300

    cleaned code some comments added performance profile on out.txt

--- src/demod/pttA2Demod.c
+++ src/demod/pttA2Demod.c
@@ -23,24 +23,19 @@ void pttA2Demod(int complex * inputSignal,int ncoInitFreq,int vgaMant,int vgaExp
 		*/
 		for(i0=0;i0<smplPerSymb;i0++){
 			inputBlock[i0] = inputSignal[i0+smplPerSymb*iSymb];
-			//printf("[%d] inputBlock %f %f i\n",i0+1, creal(inputBlock[i0]),cimag(inputBlock[i0]));
 		}
 
 		/*
 		*	NCO update and LUT
 		*/
 		ncoFreq = p->ncoDFreq+ncoInitFreq;
-		//printf("InitFreq %d p->ncoDfreq %d ncoFreq %d \n", ncoInitFreq, p->ncoDFreq, ncoFreq);
 		
 		incAndOvFlow(p->thetaNco, ncoFreq, smplPerSymb, freqW, ncoTheta);
 		p->thetaNco = ncoTheta[smplPerSymb-1];
-		//printf("p->ncoTheta %d ncoTheta[] %d \n", p->thetaNco, ncoTheta[smplPerSymb-1]);
 		
 		for(i0=0;i0<smplPerSymb;i0++){			
 			lutAddr = ncoTheta[i0] >> abs(thetaW-freqW);
-			//printf("lutAddr: %d ncoLut %f %f i\n", lutAddr, creal(ncoLut[lutAddr]),cimag(ncoLut[lutAddr]));
 			ncoSignal[i0] = creal(ncoLut[lutAddr])-cimag(ncoLut[lutAddr])*I;
-			//printf("[%d] ncoSignal %f %f i\n",i0, creal(ncoSignal[i0]),cimag(ncoSignal[i0]));
 		}		
 
 		/*
@@ -51,7 +46,6 @@ void pttA2Demod(int complex * inputSignal,int ncoInitFreq,int vgaMant,int vgaExp
 			cplxMult[i0] = inputBlock[i0]*ncoSignal[i0];
 			cplxMult[i0] = ((float complex) cplxMult[i0])*pow(2,-(ncoAmpW-1));
 			cplxMult[i0] = floor(creal(cplxMult[i0]))+floor(cimag(cplxMult[i0]))*I;
-			//printf("[%d] cplxMult %f %f i\n",i0, creal(cplxMult[i0]),cimag(cplxMult[i0]));
 		}
 
 		/*
@@ -59,18 +53,12 @@ void pttA2Demod(int complex * inputSignal,int ncoInitFreq,int vgaMant,int vgaExp
 		*/
 		cicFilterCplxStep(cplxMult,str,mfSignal,deciRate,delayIdx,smplPerSymb);
 
-		// for (i0=0;i0<nSymb;i0++){
-		// 	printf("[%d]cic %f %f i\n",i0,creal(mfSignal[i0]),cimag(mfSignal[i0]));
-		// }
 		/*
 		*	VGA input mfSignal output demodSignal
 		*/
-		//printf("MANT %d EXP %d\n",vgaMant,vgaExp);
 		for(i0=0;i0<smplPerSymb/deciRate;i0++){
 			vgaSignal[i0] = floor((creal(mfSignal[i0])*vgaMant)*pow(2,vgaExp-vgaMantW))+floor((cimag(mfSignal[i0])*vgaMant)*pow(2,vgaExp-vgaMantW))*I;
-			//printf("[%d]vga %f %f i\n",i0,creal(vgaSignal[i0]),cimag(vgaSignal[i0]));
 			demodSignal[i0] = cimag(vgaSignal[i0]);
-			//printf("demodSignal %d\n",demodSignal[i0]);
 		}
 		/*
 		*	Timer recovering SAMPLER
@@ -79,7 +67,6 @@ void pttA2Demod(int complex * inputSignal,int ncoInitFreq,int vgaMant,int vgaExp
 		sampler(demodSignal, str_smp, str1);
 		p->symbOut[iSymb] = str_smp->symbOut;
 		p->symbLock[iSymb] = str_smp->symbLock;
-		//printf("[%d] %d\n",str_smp->symbLock,str_smp->symbOut);
 
 		/*
 		*	CIC2
@@ -88,12 +75,9 @@ void pttA2Demod(int complex * inputSignal,int ncoInitFreq,int vgaMant,int vgaExp
 		for (i0 = 0;i0<deciRateSmp;i0++){
 			deciSignal = deciSignal+vgaSignal[i0];
 		}
-		//printf("deciSignal %f %f i\n",creal(deciSignal),cimag(deciSignal));
 		int absDeciSignal = cabsf(deciSignal);
 		int angDeciSignal = atan2(cimag(deciSignal),creal(deciSignal))*pow(2,cordicW-1)/PI;
-		//printf("|%d| /_ %d\n",absDeciSignal,angDeciSignal);
 		int theta = angDeciSignal*pow(2,(thetaW-cordicW));
-		//printf("theta %d\n",theta);
 		/*
 		*	PLL Loop Filter
 		*/
@@ -102,6 +86,5 @@ void pttA2Demod(int complex * inputSignal,int ncoInitFreq,int vgaMant,int vgaExp
 		p->lfAcc +=kiUInt*theta;
 		int lpf = piAdd>>(abs(freqW-thetaW-kpExp));
 		p->ncoDFreq = lpf;
-		//printf("piAdd %d | p->lfAcc %d | lpf %d \n",piAdd, p->lfAcc,lpf);
 	}
 }
